79744821ca28288bba8b4bedb5bea705b0cd3bb5
Josh <jvaage@protonmail.com>
2023-08-11 21:07:56 -0700

    tuning

--- app/(website)/about/about.js
+++ app/(website)/about/about.js
@@ -14,49 +14,6 @@ export default function About({ authors, settings }) {
           Welcome to Pirate Wires.
         </p>
       </div>
-      <div className="text-center">
-        <p className="mx-auto prose text-center dark:prose-invert mt-14">If you made it this far, you probably already know who I am — the disembodied Ulysses S. Grant guy who can't shut up on Twitter. You maybe even know I'm a billionaire media mogul, and the former mayor of San Francisco (one of these things is true). If you've dug in particularly deep, you know I'm a writer who loves a good story. I've been working in tech for over ten years, and living on the internet for most of my life. I have opinions. I have a team. My team has opinions.</p>
-
-
-      </div>
-
-      <div className="grid grid-cols-3 gap-5 mt-6 mb-16 md:mt-16 md:mb-32 md:gap-16">
-        {authors.slice(0, 5).map(author => {
-          const imageProps = urlForImage(author?.image) || null;
-          return (
-            <div
-              key={author._id}
-              className="relative overflow-hidden rounded-md aspect-square odd:translate-y-10 odd:md:translate-y-16">
-              <Link href={`/author/${author.slug}`}>
-                <Image
-                  src={imageProps.src}
-                  alt={author.name || " "}
-                  fill
-                  sizes="(max-width: 320px) 100vw, 320px"
-                  className="object-cover"
-                />
-              </Link>
-            </div>
-          );
-        })}
-      </div>
-
-      <div className="mx-auto prose text-center dark:prose-invert mt-14">
-        <p>
-          We provide real-time connectivity to enable software
-          providers and financial institutions to build integrated
-          products for their small business customers.
-        </p>
-        <p>
-          Our API infrastructure is leveraged by clients ranging from
-          lenders to corporate card providers and business forecasting
-          tools, with use cases including automatic reconciliation,
-          business dashboarding, and loan decisioning.
-        </p>
-        <p>
-          <Link href="/contact">Get in touch</Link>
-        </p>
-      </div>
     </Container >
   );
 }
