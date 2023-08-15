62df91114b84372e0ff9c2c3abf36d7f4048ec34
Josh <jvaage@protonmail.com>
2023-08-15 09:30:56 -0700

    chore: sanity

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,9 +1,17 @@
-import { defineCliConfig } from "sanity/cli";
-import { projectId, dataset } from "./lib/sanity/config";
+import { projectId, dataset } from './lib/sanity/config';
+import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
     projectId: projectId,
     dataset: dataset
-  }
+  },
+  graphql: [
+    {
+      playground: false,
+      tag: 'experiment',
+      workspace: 'staging',
+      id: 'schema-experiment'
+    }
+  ]
 });
