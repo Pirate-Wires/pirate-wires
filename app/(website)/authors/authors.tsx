7f6a19a3c4d13084ef127b5dff3a361020dd1610
Josh <jvaage@protonmail.com>
2023-08-27 17:38:52 -0700

    fix: make seperate component for authorCards

--- app/(website)/authors/authors.tsx
+++ app/(website)/authors/authors.tsx
@@ -5,17 +5,22 @@ import Link from "next/link";
 import Container from "@/components/container";
 import { urlForImage } from "@/lib/sanity/image";
 import { notFound } from "next/navigation";
-import AuthorCard from "@/components/blog/authorCard";
+import AuthorCard from "@/components/blog/authorCardAuthorsPage";
 
 // get all authors from sanity 
 
 export default function Authors({ authors }) {
+  console.log(authors);
+
   return (
     <Container>
       <h1>Authors</h1>
       <div className="grid grid-cols-2 gap-4">
         {authors.map((author) => (
-          <AuthorCard key={author.slug} author={author} />
+          <>
+            <AuthorCard key={author.slug} author={author} />
+
+          </>
         ))}
       </div>
     </Container>
