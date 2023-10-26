ce6d2fe379d078e2e5228abb35a15d4546839e65
Josh <jvaage@protonmail.com>
2023-10-26 08:43:09 -0700

    stroke-width becomes strokeWidth

--- components/latestWriters.tsx
+++ components/latestWriters.tsx
@@ -4,7 +4,7 @@ import { urlForImage } from "@/lib/sanity/image";
 import styles from "./_styles/latestWriters.module.scss"
 import PostList from "@/components/postlist";
 import AuthorTile from "@/components/authorTile";
-export default function LatestWriters({latestAuthors}) {
+export default function LatestWriters({ latestAuthors }) {
   // const imageProps = post?.mainImage
   //   ? urlForImage(post?.mainImage)
   //   : null;
@@ -27,14 +27,14 @@ export default function LatestWriters({latestAuthors}) {
             href={`/authors`}>
             See all
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="17" viewBox="0 0 18 17" fill="none">
-              <path d="M0.744407 8.61832H16.6441M16.6441 8.61832L8.69427 0.668457M16.6441 8.61832L8.69427 16.5682" stroke="#E3E3E3" stroke-width="0.699553"/>
+              <path d="M0.744407 8.61832H16.6441M16.6441 8.61832L8.69427 0.668457M16.6441 8.61832L8.69427 16.5682" stroke="#E3E3E3" strokeWidth="0.699553" />
             </svg>
           </Link>
         </div>
 
         <div className={styles.bottom}>
           {latestAuthors.map(author => (
-            <AuthorTile key={author.name} authorData={author}/>
+            <AuthorTile key={author.name} authorData={author} />
           ))}
         </div>
       </div>
