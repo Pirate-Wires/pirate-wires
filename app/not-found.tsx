7d077a2be4a1fb090be07957b59435204cc1eefb
joshkirk <joshgkirk@gmail.com>
2023-10-30 15:18:50 -0600

    404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,42 @@
+import {getGlobalFields} from "@/lib/sanity/client";
+import React from "react";
+import Navigation from "@/components/navigation";
+import Footer from "@/components/footer";
+import styles from "@/styles/pages/notFound.module.scss"
+import Image from "next/image";
+import Link from "next/link";
+
+export default async function NotFound() {
+  const globalFields = await getGlobalFields();
+  return (
+    <div className="colorWrapper" style={{
+    "--color": "#E3E3E3",
+    "--bgColor": "#060606",
+  } as React.CSSProperties}>
+    <Navigation globalFields={globalFields} />
+    <section className={styles.notFound}>
+      <div className={styles.imagesWrapper}>
+        <div className={styles.imageWrapper}>
+          <Image src={'/img/handwire1.jpg'}
+                 className="object-cover"
+                 sizes="(max-width: 800px) 100vw, 800px"
+                 priority
+                 fill
+          />
+        </div>
+        <div className={styles.imageWrapper}>
+          <Image src={'/img/handwire2.jpg'}
+                 className="object-cover"
+                 sizes="(max-width: 800px) 100vw, 800px"
+                 priority
+                 fill
+          />
+        </div>
+      </div>
+      <h1>404 – Wires Disconnected</h1>
+      <Link href={"/"} className={`${styles.btn} btn square`}>Go to home</Link>
+    </section>
+    <Footer globalFields={globalFields} />
+  </div>
+  )
+}
