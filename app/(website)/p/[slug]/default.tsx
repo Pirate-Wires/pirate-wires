e60b7f01cf7b6574ba7cce057bfa8b2efc680035
joshkirk <joshgkirk@gmail.com>
2023-10-30 12:28:15 -0600

    format article page

--- app/(website)/p/[slug]/default.tsx
+++ app/(website)/p/[slug]/default.tsx
@@ -12,16 +12,11 @@ import Container from "@/components/container";
 import { notFound } from "next/navigation";
 import { PortableText } from "@/lib/sanity/plugins/portabletext";
 import { urlForImage } from "@/lib/sanity/image";
-import CategoryLabel from "@/components/blog/category";
-import AuthorCard from "@/components/blog/authorCard";
 import {useDateFormatter} from "@/hooks/useDateFormatter";
-
+import styles from "@/styles/pages/article.module.scss"
 import CommentSection from '@/lib/supabase-comments/components/comments/CommentSection';
-import SidebarComments from '@/lib/supabase-comments/components/comments/SidebarComments';
-import MessageBubbleButton from '@/lib/supabase-comments/components/comments/MessageBubbleButton';
-import HeartButton from '@/lib/supabase-comments/components/comments/HeartButton';
-import { ModalProvider } from '@/lib/supabase-comments/hooks/use-modal';
-import Github from '@/lib/supabase-comments/icons/Github';
+import React from "react";
+import AuthorTile from "@/components/authorTile";
 
 export default async function Post(props) {
   const [session, userDetails, products, subscription] = await Promise.all([
@@ -49,92 +44,60 @@ export default async function Post(props) {
     notFound();
   }
 
-  const imageProps = post?.mainImage
-    ? urlForImage(post?.mainImage)
-    : null;
-
-  const AuthorimageProps = post?.author?.image
-    ? urlForImage(post.author.image)
-    : null;
-
   console.log('post', post)
 
+  // const [loaded, setLoaded] = useState(false)
+  // const onLoad = () => {
+  //   setTimeout(() => {
+  //     setLoaded(true)
+  //   }, 250)
+  // }
 
   return (
     <>
-      <Container className="!pt-0">
-        <div className="mx-auto max-w-screen-md ">
-          <div className="flex justify-center">
-            <CategoryLabel categories={post.categories} />
-          </div>
-
-          <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl lg:leading-snug">
+      <section className={styles.articleHero}>
+        <div className={`${styles.imageWrapper} imageWrapper`}>
+          {/*{!loaded &&*/}
+          {/*  <img src={post?.mainImage.blurDataURL} alt="" decoding="async" loading="lazy" className="cover-image"/>*/}
+          {/*}*/}
+          <img src={post?.mainImage.blurDataURL} alt="" decoding="async" loading="lazy" className="cover-image"/>
+          <picture>
+            <source srcSet={`${post?.mainImage.asset.url}?auto=format&w=600&q=90, ${post?.mainImage.asset.url}?auto=format&w=800&q=90 2x`} media="(min-width: 768px)" />
+            <source srcSet={`${post?.mainImage.asset.url}?auto=format&w=550&q=100`} media="(max-width: 767px)" />
+            {/*<img alt="" decoding="async" loading="lazy" className="cover-image" onLoad={onLoad}/>*/}
+            <img alt="" decoding="async" loading="lazy" className="cover-image"/>
+          </picture>
+          <figcaption>
+            {post?.mainImage.caption && (
+              <span>
+                {post?.mainImage.caption}
+              </span>
+            )}
+          </figcaption>
+        </div>
+        <div className={styles.right}>
+          <h1>
             {post.title}
           </h1>
-
-          <div className="mt-3 flex justify-center space-x-3 text-gray-500 ">
-            <div className="flex items-center gap-3">
-              <div className="relative h-10 w-10 flex-shrink-0">
-                {AuthorimageProps && (
-                  <Link href={`/author/${post.author.slug.current}`}>
-                    <Image
-                      src={AuthorimageProps.src}
-                      alt={post?.author?.name}
-                      className="rounded-full object-cover"
-                      fill
-                      sizes="40px"
-                    />
-                  </Link>
-                )}
-              </div>
-              <div>
-                <p className="text-gray-800 dark:text-gray-400">
-                  <Link href={`/author/${post.author.slug.current}`}>
-                    {post.author.name}
-                  </Link>
-                </p>
-                <div className="flex items-center space-x-2 text-sm">
-                  <time
-                    className="text-gray-500 dark:text-gray-400"
-                    dateTime={post?.publishedAt || post._createdAt}>
-                    {useDateFormatter(post?.publishedAt || post._createdAt)}
-                  </time>
-                  <span>· {post.estReadingTime || "5"} min read</span>
-                </div>
-              </div>
-            </div>
+          <p>
+            post.preface
+          </p>
+          <div className={styles.bottom}>
+            <Link href={`/author/${post.author.slug.current}`}>
+              {post.author.name}
+            </Link>
+            <p>{useDateFormatter(post?.publishedAt || post._createdAt, true)}</p>
           </div>
         </div>
-      </Container>
+      </section>
 
+      <section className={styles.postBody}>
+        <div className={styles.richText}>
+          {post.body && <PortableText value={post.body} />}
+        </div>
+      </section>
 
-
-      <div className="relative z-0 mx-auto aspect-video max-w-screen-lg overflow-hidden lg:rounded-lg">
-        {imageProps && (
-          <Image
-            src={imageProps.src}
-            alt={post.mainImage?.alt || "Thumbnail"}
-            loading="eager"
-            fill
-            sizes="100vw"
-            className="object-cover"
-          />
-        )}
-      </div>
-
-      <Container>
-        <article className="mx-auto max-w-screen-md ">
-          <div className="prose mx-auto my-3 dark:prose-invert prose-a:text-blue-500">
-            {post.body && <PortableText value={post.body} />}
-          </div>
-        </article>
-
-      </Container>
-
-
-
-
-      {/* Paid Content */}
+      {/* Paid Content
       <Container>
         <article className="mx-auto max-w-screen-md ">
           <div className="prose mx-auto my-3 dark:prose-invert prose-a:text-blue-500 border p-12">
@@ -157,8 +120,8 @@ export default async function Post(props) {
             {post.paidContent && <PortableText value={post.paidContent} />}
           </div>
         </article>
-        {post.author && <AuthorCard author={post.author} />}
-      </Container>
+        {post.author && <AuthorTile author={post.author} />}
+      </Container>*/}
 
       <Container>
         <section className="mx-auto max-w-screen-md border py-24">
@@ -168,19 +131,3 @@ export default async function Post(props) {
     </>
   );
 }
-
-const MainImage = ({ image }) => {
-  return (
-    <div className="mb-12 mt-12 ">
-      {/* @ts-ignore */}
-      <Image {...urlForImage(image)} alt={image.alt || "Thumbnail"} />
-      <figcaption className="text-center ">
-        {image.caption && (
-          <span className="text-sm italic text-gray-600 dark:text-gray-400">
-            {image.caption}
-          </span>
-        )}
-      </figcaption>
-    </div>
-  );
-};
