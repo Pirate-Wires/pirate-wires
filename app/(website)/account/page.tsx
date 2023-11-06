e2f2b4317df799432379cda85088d60ebca5fc35
Josh <71414784+franswan@users.noreply.github.com>
2023-11-05 20:34:10 -0800

    account sections

--- app/(website)/account/page.tsx
+++ app/(website)/account/page.tsx
@@ -1,21 +1,17 @@
-import ManageSubscriptionButton from './ManageSubscriptionButton';
-import Pricing from '@/components/Pricing';
 import {
   getSession,
   getUserDetails,
   getSubscription,
   getActiveProductsWithPrices
 } from '@/app/(website)/supabase-server';
-import Button from '@/components/ui/Button';
 import type { Database } from '@/types/supabase';
 import { createServerActionClient } from '@supabase/auth-helpers-nextjs';
 import { revalidatePath } from 'next/cache';
 import { cookies } from 'next/headers';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
-import { ReactNode } from 'react';
 import Navbar from '@/components/ui/Navbar';
-import Navigation from '@/components/navigation';
+
 
 export default async function Account() {
   const [session, userDetails, products, subscription] = await Promise.all([
@@ -24,11 +20,6 @@ export default async function Account() {
     getActiveProductsWithPrices(),
     getSubscription()
   ]);
-
-  // console.log('session', session);
-  // console.log('userDetails', userDetails);
-  // console.log('products', products);
-  // console.log('subscription', subscription);
 
   const user = session?.user;
   console.log('user', user?.email);
@@ -80,28 +71,83 @@ export default async function Account() {
   };
 
   return (
-    <section className="mb-32">
+    <div>
 
       <Navbar />
 
+      <hr />
 
-      <Pricing
-        session={session}
-        user={session?.user}
-        products={products}
-        subscription={subscription}
-      />
+      {/* nav */}
+      <ul>
+        <li>My details</li>
+        <li>Email Preferences</li>
+        <li>Commenting</li>
+        <li>Subscription & Billing</li>
+        <li>Sign Out</li>
+      </ul>
 
-      <div className="p-4">
-        <Card
-          title="Your Plan"
-          description={
-            subscription
+      <hr />
+
+      <section>
+        <h3>My Details</h3>
+        <form>
+          <label htmlFor="firstName">First Name:</label>
+          <input
+            type="text"
+            id="firstName"
+            name="firstName"
+            required
+          />
+          <br />
+          <label htmlFor="lastName">Last Name:</label>
+          <input
+            type="text"
+            id="lastName"
+            name="lastName"
+            required
+          />
+          <br />
+          <button type="submit">Save</button>
+        </form>
+        <form id="emailForm" action={updateEmail}>
+          <label htmlFor="firstName">Update your email:</label>
+          <input
+            type="text"
+            id="email"
+            name="email"
+            value={user ? user.email : ''}
+            required
+          />
+          <br />
+          <button type="submit">Save</button>
+        </form>
+      </section>
+
+      <hr />
+
+      <section>
+        <h3>Email Preferences</h3>
+        <p>If subscribed: Email notifications are currently enabled.</p>
+      </section>
+
+      <hr />
+
+      <section>
+        <h3>Commenting</h3>
+        <p>If subscribed: Commenting is currently enabled.</p>
+        notify me of replies to my comments
+      </section>
+
+      <hr />
+
+      <section>
+        <div>
+          <h3>Subscription & Billing</h3>
+          <p>
+            {subscription
               ? `You are currently on the ${subscription?.prices?.products?.name} plan.`
-              : 'You are not currently subscribed to any plan.'
-          }
-          footer={<ManageSubscriptionButton session={session} />}
-        >
+              : 'You are not currently subscribed to any plan.'}
+          </p>
           <div className="mt-8 mb-4 text-xl font-semibold">
             {subscription ? (
               `${subscriptionPrice}/${subscription?.prices?.interval}`
@@ -109,96 +155,23 @@ export default async function Account() {
               <Link href="/">Choose your plan</Link>
             )}
           </div>
-        </Card>
-        <Card
-          title="Your Name"
-          description="Please enter your full name, or a display name you are comfortable with."
-          footer={
-            <div className="flex flex-col items-start justify-between sm:flex-row sm:items-center">
-              <p className="pb-4 sm:pb-0">64 characters maximum</p>
-              <Button
-                variant="slim"
-                type="submit"
-                form="nameForm"
-                disabled={true}
-              >
-                {/* WARNING - In Next.js 13.4.x server actions are in alpha and should not be used in production code! */}
-                Update Name
-              </Button>
-            </div>
-          }
-        >
-          <div className="mt-8 mb-4 text-xl font-semibold">
-            <form id="nameForm" action={updateName}>
-              <input
-                type="text"
-                name="name"
-                className="w-full px-3 py-2 border rounded-xs outline-none focus:border-gray-300 focus:shadow-sm dark:bg-gray-900 dark:border-gray-600 dark:focus:border-white"
-                defaultValue={userDetails?.full_name ?? ''}
-                placeholder="Your name"
-                maxLength={64}
-              />
-            </form>
-          </div>
-        </Card>
+        </div>
+      </section>
 
-        <Card
-          title="Your Email"
-          description="Please enter the email address you want to use to login."
-          footer={
-            <div className="flex flex-col items-start justify-between sm:flex-row sm:items-center">
-              <p className="pb-4 sm:pb-0">
-                We will email you to verify the change.
-              </p>
-              <Button
-                variant="slim"
-                type="submit"
-                form="emailForm"
-                disabled={true}
-              >
-                {/* WARNING - In Next.js 13.4.x server actions are in alpha and should not be used in production code! */}
-                Update Email
-              </Button>
-            </div>
-          }
-        >
-          <div className="mt-8 mb-4 text-xl font-semibold">
+      <hr />
 
-            <form id="emailForm" action={updateEmail}>
-              <input
-                type="text"
-                name="email"
-                className="w-full px-3 py-2 border rounded-xs outline-none focus:border-gray-300 focus:shadow-sm dark:bg-gray-900 dark:border-gray-600 dark:focus:border-white"
-                defaultValue={user ? user.email : ''}
-                placeholder="Your email"
-                maxLength={64}
-              />
-            </form>
-          </div>
-        </Card>
-      </div>
-    </section>
-  );
-}
+      {/* <Pricing
+        session={session}
+        user={session?.user}
+        products={products}
+        subscription={subscription}
+      /> */}
 
-interface Props {
-  title: string;
-  description?: string;
-  footer?: ReactNode;
-  children: ReactNode;
-}
 
-function Card({ title, description, footer, children }: Props) {
-  return (
-    <div className="w-full max-w-3xl m-auto my-8 border dark:border-gray-800 rounded-xs p">
-      <div className="px-5 py-4">
-        <h3 className="mb-1 text-xl">{title}</h3>
-        <p className="text-sm">{description}</p>
-        {children}
-      </div>
-      <div className="p-4 border-t dark:border-gray-800">
-        {footer}
-      </div>
     </div>
   );
 }
+
+
+
+
--- components/ui/Navbar/Navbar.tsx
+++ components/ui/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { createServerSupabaseClient } from '@/app/(website)/supabase-server';
 import SignOutButton from './SignOutButton';
+import React from 'react';
 
 export default async function Navbar() {
   const supabase = createServerSupabaseClient();
@@ -8,27 +9,81 @@ export default async function Navbar() {
     data: { user }
   } = await supabase.auth.getUser();
 
-  // console.log('user', user);
+  // function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+  //   event.preventDefault();
+  //   console.log('submit');
+  // }
+
+  // const [firstName, setFirstName] = React.useState('');
+  // const [lastName, setLastName] = React.useState('');
+
+  // function handleFirstNameChange(event: React.FormEvent<HTMLInputElement>): void {
+  //   throw new Error('Function not implemented.');
+  // }
+
+  // function handleLastNameChange(event: React.FormEvent<HTMLInputElement>): void {
+  //   throw new Error('Function not implemented.');
+  // }
 
   return (
-    <nav>
-      {/* <div>
-        <p>user: {user?.email}</p>
-        <p>user: {user?.id}</p>
-        {user?.user_metadata.email}
-        <pre>{user?.role}</pre>
-        <img src={user?.user_metadata.avatar_url} />
-      </div> */}
 
-      <div className="flex justify-end flex-1 space-x-8">
+    <div>
+
+      <div>
         {user ? (
           <SignOutButton />
         ) : (
-          <Link href="/sign-in" className={s.link}>
+          <Link href="/sign-in">
             Sign in
           </Link>
         )}
       </div>
-    </nav>
+
+      <hr />
+      <ul>
+        <li>
+          <p>user email: {user?.email}</p>
+        </li>
+        <li>
+          <p>user id: {user?.id}</p>
+        </li>
+        <li>{user?.user_metadata.email}</li>
+        <li>
+          <pre>{user?.role}</pre>
+        </li>
+        <li>
+          <img
+            src={user?.user_metadata.avatar_url}
+            style={{ width: '75px' }}
+          />
+        </li>
+      </ul>
+
+      {/* <form onSubmit={handleSubmit}>
+        <label htmlFor="firstName">First Name:</label>
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          value={firstName}
+          onChange={handleFirstNameChange}
+          required
+        />
+        <br />
+        <label htmlFor="lastName">Last Name:</label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={lastName}
+          onChange={handleLastNameChange}
+          required
+        />
+        <br />
+        <button type="submit">Save</button>
+      </form> */}
+
+    </div>
+
   );
 }
