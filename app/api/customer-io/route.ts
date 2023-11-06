4039180b12e1be23dd73fc0b753ff070654bd7ff
Jared Redford <jaredrmain@outlook.com>
2023-11-06 07:15:51 -0800

    Create supabase user when customer.io user created

--- app/api/customer-io/route.ts
+++ app/api/customer-io/route.ts
@@ -1,18 +1,22 @@
 // app/api/customer-io/route.ts
 import crypto from 'crypto';
 import { trackerCio, apiCio } from '../../../lib/cioClient';
+import {
+  createAuthUser
+} from '@/utils/supabase-admin';
 
 export async function PUT(req: Request) {
   const body = await req.json();
   const { email, subscription } = body;
 
   try {
+    await createAuthUser(email);
+
     const response = await apiCio.getCustomersByEmail(email!);
     const customer = response.results[0];
+    const topics = ['Wires', 'The Industry', 'The White Pill'];
 
     if(customer) {
-      const topics = ['Wires', 'The Industry', 'The White Pill'];
-
       trackerCio.identify(`cio_${customer.cio_id}`, {
         'cio_subscription_preferences' : `{
           "topics": {
@@ -29,6 +33,13 @@ export async function PUT(req: Request) {
         .slice(0, 12);
       trackerCio.identify(cio_id, {
         email,
+        'cio_subscription_preferences' : `{
+          "topics": {
+            ${topics.map((item, index) =>
+                `"topic_${index + 1}": ${subscription.indexOf(item) > -1}`)
+              .join(',')}
+          }
+        }`
       });
     }
 
