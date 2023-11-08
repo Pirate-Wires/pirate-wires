3fdcd810327c763df51410c869e441002ac1703c
Jared Redford <jaredrmain@outlook.com>
2023-11-08 08:13:18 -0800

    Add simple sanity sync

---
+++ scripts/syncSanity.js
@@ -0,0 +1,56 @@
+require('dotenv').config({ path: './.env.local' });
+
+const { createClient: createSupabaseClient } = require('@supabase/supabase-js');
+const { createClient: createSanityClient } = require('next-sanity');
+
+const projectId =
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ||
+  process.env.SANITY_STUDIO_PROJECT_ID;
+
+const dataset =
+  process.env.NEXT_PUBLIC_SANITY_DATASET || "production";
+
+const apiVersion =
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION || "2023-03-25";
+
+const useCdn = process.env.NODE_ENV === "production";
+
+const client = createSanityClient({
+  projectId,
+  dataset,
+  apiVersion,
+  useCdn
+});
+
+const supabaseAdmin = createSupabaseClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+
+const createSupabasePost = async (postData) => {
+  const { data, error } = await supabaseAdmin
+    .from('posts')
+    .insert(postData)
+  if(error) {
+    console.error(`Error inserting post: ${error.message}`);
+    return;
+  }
+  console.log(`Insert post successfully: ${data}`);
+}
+
+client.fetch('*[ _type == "post"]') // Adjust query based on your needs
+  .then(data => {
+    data.forEach(async (item) => {
+      const postData = {
+        sanity_id: item._id,
+        slug: item.slug?.current,
+        title: item.title,
+        content: 'content',
+        parentId: null,
+        isPublished: true,
+        authorId: `4d83545c-72a9-4dbe-98ad-45785b5fbeb3`,
+      }
+      //console.log(postData);
+      await createSupabasePost(postData);
+    });
+  });
