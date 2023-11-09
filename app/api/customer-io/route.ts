491001b9a72b2699a451638d2ecc9d0c5795e3d8
Jared Redford <jaredrmain@outlook.com>
2023-11-08 19:26:57 -0800

    Fix subscription in each section

--- app/api/customer-io/route.ts
+++ app/api/customer-io/route.ts
@@ -1,7 +1,8 @@
 // app/api/customer-io/route.ts
 import crypto from 'crypto';
-import { trackerCio, apiCio } from '../../../lib/cioClient';
+import { trackerCio, apiCio, getCustomerId } from '@/lib/cioClient';
 import { createAuthUser } from '@/utils/supabase-admin';
+import { getCustomerSubscription } from '@/utils/cio-api';
 
 export async function PUT(req: Request) {
   const body = await req.json();
@@ -16,6 +17,60 @@ export async function PUT(req: Request) {
 
     if (customer) {
       trackerCio.identify(`cio_${customer.cio_id}`, {
+        cio_subscription_preferences: `{
+          "topics": {
+            ${topics
+              .map(
+                (item, index) =>
+                  `"topic_${index + 1}": ${subscription.indexOf(item) > -1}`
+              )
+              .join(',')}
+          }
+        }`
+      });
+    } else {
+      const cio_id = crypto
+        .createHash('sha256')
+        .update(email)
+        .digest('hex')
+        .slice(0, 12);
+      trackerCio.identify(cio_id, {
+        email,
+        cio_subscription_preferences: `{
+          "topics": {
+            ${topics
+              .map(
+                (item, index) =>
+                  `"topic_${index + 1}": ${subscription.indexOf(item) > -1}`
+              )
+              .join(',')}
+          }
+        }`
+      });
+    }
+
+    return new Response(JSON.stringify({ success: true }), { status: 200 });
+  } catch (err) {
+    return new Response(`Error: ${err.message}`, { status: 500 });
+  }
+}
+
+export async function POST(req: Request) {
+  const body = await req.json();
+  const { email, section } = body;
+
+  try {
+    await createAuthUser(email);
+
+    const cioId = await getCustomerId(email);
+    const customerSubscription = await getCustomerSubscription(cioId);
+    const subscription = customerSubscription.indexOf(section) > -1 ? customerSubscription : [...customerSubscription, section];
+    const topics = ['Wires', 'The Industry', 'The White Pill'];
+
+    console.log(email, subscription);
+
+    if (cioId) {
+      trackerCio.identify(`cio_${cioId}`, {
         cio_subscription_preferences: `{
           "topics": {
             ${topics
--- lib/cioClient.ts
+++ lib/cioClient.ts
@@ -8,3 +8,14 @@ const TRACKING_API_KEY = process.env.CUSTOMER_IO_TRACKING_API_KEY as string;
 export const trackerCio = new TrackClient(SITE_ID, TRACKING_API_KEY, { region: RegionUS });
 export const apiCio = new APIClient(SITE_API_KEY, { region: RegionUS });
 
+export const getCustomerId = async (email: string) => {
+  const response = await apiCio.getCustomersByEmail(email);
+  const customer = response.results[0];
+
+  if(!customer) {
+    return null;
+  }
+
+  return customer.cio_id;
+}
+
