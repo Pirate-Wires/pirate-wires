4f8f8f8e1233ca7762603bebb7fde16a8bfcd3b9
Jared Redford <jaredrmain@outlook.com>
2023-11-16 01:17:34 -0800

    Remove stripe customers with same email

---
+++ scripts/removeDuplicatedStripeCustomer.js
@@ -0,0 +1,56 @@
+require('dotenv').config({ path: './.env.local' })
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+async function getAllStripeCustomers() {
+  const customers = [];
+  let lastId = undefined;
+
+  while (true) {
+    const response = await stripe.customers.list({ limit: 100, starting_after: lastId });
+    customers.push(...response.data);
+
+    if (response.data.length === 0) {
+      break;
+    }
+
+    lastId = response.data[response.data.length - 1].id;
+  }
+
+  return customers;
+}
+
+async function removeDuplicateCustomers() {
+  const allCustomers = await getAllStripeCustomers();
+  const customersByEmail = new Map();
+
+  // Grouping customers by email
+  allCustomers.forEach(customer => {
+    if (customer.email) {
+      if (!customersByEmail.has(customer.email)) {
+        customersByEmail.set(customer.email, []);
+      }
+      customersByEmail.get(customer.email).push(customer);
+    }
+  });
+
+  for (const [email, customers] of customersByEmail) {
+    if (customers.length > 1) {
+      // Prioritize customers with a default_payment_method
+      const sortedCustomers = customers.sort((a, b) => {
+        const aHasPaymentMethod = a.invoice_settings.default_payment_method != null;
+        const bHasPaymentMethod = b.invoice_settings.default_payment_method != null;
+        return bHasPaymentMethod - aHasPaymentMethod || a.created_at - b.created_at;
+      });
+
+      // Deleting all other customers
+      for (const customer of sortedCustomers.slice(1)) {
+        await stripe.customers.del(customer.id);
+        console.log(`Deleted duplicate customer with email, ID: ${customer.email}, ${customer.id}`);
+      }
+    }
+  }
+}
+
+removeDuplicateCustomers()
+  .then(() => console.log('Duplicate removal process completed.'))
+  .catch(error => console.error(error));
