59bf2f7e613876ec59f563eb615d0e68750fe801
Jared Redford <jaredrmain@outlook.com>
2023-11-20 23:07:52 -0800

    Fix supabase functions for deployment

--- supabase/functions/otp/index.ts
+++ supabase/functions/otp/index.ts
@@ -1,19 +1,19 @@
 // supabse/functions/otp/index.ts
 // c.io transactional email for OTP
 // transactional email template id: 3
-export default sendOTP((event) => {
-  if (event.type === 'user_updated') {
-    // Generate OTP token
-    const { data } = await supabase.rpc('generate_otp_token', {
-      email: event.user.email
-    });
+export const sendOTP = (event) => {
+  // if (event.type === 'user_updated') {
+  //   // Generate OTP token
+  //   const { data } = await supabase.rpc('generate_otp_token', {
+  //     email: event.user.email
+  //   });
 
-    // Send OTP code email with Customer.IO
-    if (data) {
-      sendOtpEmail(event.user.email, data.token);
-    }
-  }
-});
+  //   // Send OTP code email with Customer.IO
+  //   if (data) {
+  //     sendOtpEmail(event.user.email, data.token);
+  //   }
+  // }
+};
 
 async function sendOtpEmail(email, token) {
   // TODO - Send email confirmation code email
