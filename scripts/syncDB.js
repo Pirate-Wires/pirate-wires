d53a9bd0f356b41ec53b72dd1dc614910130e234
Jared Redford <jaredrmain@outlook.com>
2023-11-22 02:12:15 -0800

    Add sync all three dbs

---
+++ scripts/syncDB.js
@@ -0,0 +1,24 @@
+const { syncSupabaseWithCio } = require('./syncSupabaseWithCio');
+const { syncSupabaseWithStripe } = require('./syncSupabaseWithStripe');
+const { syncCioWithSupabase } = require('./syncCioWithSupabase');
+const { syncStripeWithSupabase } = require('./syncStripeWithSupabase');
+
+const syncDB = async () => {
+  try {
+    await syncSupabaseWithCio();
+    await syncSupabaseWithStripe();
+    await syncCioWithSupabase();
+    await syncStripeWithSupabase();
+
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+syncDB();
+
+module.exports = {
+  syncDB
+};
