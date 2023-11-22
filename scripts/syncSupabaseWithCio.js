2d4485d407ea3a28cd04ad6627fc68c6e3f73994
Jared Redford <jaredrmain@outlook.com>
2023-11-22 02:11:59 -0800

    Add sync supabase with cio

---
+++ scripts/syncSupabaseWithCio.js
@@ -0,0 +1,26 @@
+const {
+  getAllSupabaseUsers,
+  getAllCioCustomers,
+  updateSupabaseFromCio,
+} = require('./utils');
+
+const syncSupabaseWithCio = async () => {
+  try {
+    const users = await getAllSupabaseUsers();
+    const customers = await getAllCioCustomers();
+
+    console.log('Start syncing Supabase users with Customer.io customers...');
+    await updateSupabaseFromCio({ users, customers });
+
+    process.exit(0);
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+};
+
+syncSupabaseWithCio();
+
+module.exports = {
+  syncSupabaseWithCio
+};
