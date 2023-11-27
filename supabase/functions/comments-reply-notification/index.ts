d54210546c5f0e9e05374bf1bd79ab61a0ebce4c
Jared Redford <jaredrmain@outlook.com>
2023-11-27 13:09:29 -0800

    Fix lint error

--- supabase/functions/comments-reply-notification/index.ts
+++ supabase/functions/comments-reply-notification/index.ts
@@ -1,20 +1,20 @@
-import {serve} from "https://deno.land/std@0.168.0/http/server.ts";
+// import {serve} from "https://deno.land/std@0.168.0/http/server.ts";
 
-serve(async req => {
-  if (req.method !== "POST") {
-    return new Response(JSON.stringify({error: "Method Not Allowed"}), {
-      status: 405,
-      headers: {"Content-Type": "application/json"},
-    });
-  }
+// serve(async req => {
+//   if (req.method !== "POST") {
+//     return new Response(JSON.stringify({error: "Method Not Allowed"}), {
+//       status: 405,
+//       headers: {"Content-Type": "application/json"},
+//     });
+//   }
 
-  const payload = await req.json();
+//   const payload = await req.json();
 
-  // Process payload
-  console.log("Webhook received:", payload);
+//   // Process payload
+//   console.log("Webhook received:", payload);
 
-  return new Response(
-    JSON.stringify({message: "Webhook received successfully!"}),
-    {status: 200, headers: {"Content-Type": "application/json"}},
-  );
-});
+//   return new Response(
+//     JSON.stringify({message: "Webhook received successfully!"}),
+//     {status: 200, headers: {"Content-Type": "application/json"}},
+//   );
+// });
