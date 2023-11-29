5b103d7919a63c9993204f519d26f5cde53d29fe
whaleen <joshuavaage@gmail.com>
2023-11-28 20:29:30 -0800

    remove experimental serverActions

--- next.config.js
+++ next.config.js
@@ -10,9 +10,6 @@ const nextConfig = {
     remotePatterns: [{hostname: "cdn.sanity.io"}],
     deviceSizes: [767, 959, 1024, 1200, 1600, 1920, 2048, 3840, 5000],
   },
-  experimental: {
-    serverActions: true,
-  },
   typescript: {
     // Set this to false if you want production builds to abort if there's type errors
     ignoreBuildErrors: process.env.VERCEL_ENV === "production",
