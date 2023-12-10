b83f17b5a669de3fe5bf04bbd12b05b1641f2765
Jared Redford <jaredrmain@outlook.com>
2023-12-10 12:38:20 -0800

    PW-158: Add email verify util function

--- utils/kickbox.ts
+++ utils/kickbox.ts
@@ -2,13 +2,13 @@ import kickbox from "kickbox";
 
 const client = kickbox.client(process.env.KICKBOX_LIVE_API_KEY).kickbox();
 
-export const checkEmail = async (email: string) => {
+export const verifyEmail = async (email: string) => {
   try {
     const response = await client.verify(email);
     const result = response.body.result === "deliverable";
-    return {data: result, error: null};
+    return { data: result, error: null };
   } catch (error) {
-    console.error(`Error checking email ${email}: ${error.message}`);
-    return {data: null, error};
+    console.error(`Error verifying email ${email}: ${error.message}`);
+    return { data: null, error };
   }
 };
