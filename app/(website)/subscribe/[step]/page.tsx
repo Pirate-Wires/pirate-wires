61559bf1b030027a3bb5060477eb71e70250eb7c
Jared Redford <jaredrmain@outlook.com>
2023-12-14 14:41:26 -0800

    PW-229: Update step three flow

--- app/(website)/subscribe/[step]/page.tsx
+++ app/(website)/subscribe/[step]/page.tsx
@@ -1,9 +1,8 @@
-import {notFound} from "next/navigation";
+import { notFound } from "next/navigation";
 
-import {getUserDetails} from "@/app/(website)/supabase-server";
-import {getGlobalFields} from "@/lib/sanity/client";
+import { getUserDetails } from "@/app/(website)/supabase-server";
+import { getGlobalFields } from "@/lib/sanity/client";
 import Navigation from "@/components/navigation";
-import Footer from "@/components/footer";
 
 import StepOne from "./StepOne";
 import StepTwo from "./StepTwo";
@@ -13,25 +12,19 @@ import styles from "@/styles/pages/subscribe.module.scss";
 import Link from "next/link";
 import React from "react";
 
-export default async function SubscribePage({params, searchParams}) {
+export default async function SubscribePage({ params, searchParams }) {
   const user = await getUserDetails();
   const globalFields = await getGlobalFields();
-  const {email, customerId} = searchParams;
+  const { email } = searchParams;
 
-  const StepSwitcher = ({step}: {step: string}) => {
+  const StepSwitcher = ({ step }: { step: string }) => {
     switch (step) {
       case "step-1":
         return <StepOne email={user?.email} />;
       case "step-2":
-        return <StepTwo email={email} customerId={customerId} />;
+        return <StepTwo email={email} />;
       case "step-3":
-        return (
-          <StepThree
-            email={email}
-            customerId={customerId}
-            subscription={user?.subscription_id!}
-          />
-        );
+        return <StepThree email={email} subscription={user?.subscription_id!} />;
       case "step-4":
         return <StepFour email={email} />;
       default:
@@ -50,24 +43,19 @@ export default async function SubscribePage({params, searchParams}) {
         } as React.CSSProperties
       }>
       <Navigation globalFields={globalFields} />
-      <section
-        className={`${styles.subscribeWrapper} flowContainer c-20 pb-20`}>
+      <section className={`${styles.subscribeWrapper} flowContainer c-20 pb-20`}>
         <StepSwitcher step={params.step} />
         <p className={`disclaimer`}>
           By continuing, you agree to the{" "}
           <Link
             target={"_blank"}
-            href={
-              "https://app.termly.io/document/terms-of-service/7109fc1e-402d-466e-9f79-fe8cbe4a2b71"
-            }>
+            href={"https://app.termly.io/document/terms-of-service/7109fc1e-402d-466e-9f79-fe8cbe4a2b71"}>
             Terms & Conditions
           </Link>{" "}
           and{" "}
           <Link
             target={"_blank"}
-            href={
-              "https://app.termly.io/document/privacy-policy/42d3d1fe-f9d0-4cc4-9685-91ce1329b836"
-            }>
+            href={"https://app.termly.io/document/privacy-policy/42d3d1fe-f9d0-4cc4-9685-91ce1329b836"}>
             Privacy Policy
           </Link>
         </p>
