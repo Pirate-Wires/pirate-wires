f9eeebce0bfc1d416bd7f14102225785ea327497
Sachin Kesiraju <kesirajusachin@gmail.com>
2023-12-16 11:43:51 -0800

    PW-133: Create guest vote with generated guestID

--- components/comments/LikeButton.tsx
+++ components/comments/LikeButton.tsx
@@ -2,6 +2,7 @@ import {invokeVote} from "@/components/comments/VoteButtons";
 import {useComments} from "@/lib/hooks/useComments";
 import {useSupabase} from "@/app/(website)/supabase-provider";
 import {CommentType} from "@/lib/utils/types";
+import { v4 } from "uuid";
 import React from "react";
 
 import styles from "@/components/_styles/comments.module.scss";
@@ -11,7 +12,7 @@ const HeartButton = (): JSX.Element => {
   const {rootComment, mutateRootComment, redirectToSignIn} = useComments();
 
   async function handleVote(): Promise<void> {
-    if (!user) return redirectToSignIn();
+    if (!user) return createGuestVote();
     if (!rootComment || !rootComment.id) return;
 
     if (rootComment.userVoteValue === 0) {
@@ -31,6 +32,49 @@ const HeartButton = (): JSX.Element => {
     }
   }
 
+  async function createGuestVote(): Promise<void> {
+    var guestId = localStorage.getItem("guestId");
+    if (!guestId) {
+      guestId = v4();
+      localStorage.setItem("guestId", guestId);
+    }
+    if (!rootComment || !rootComment.id) return;
+
+    if (rootComment.userVoteValue === 0) {
+      mutateRootComment((data: CommentType) => ({
+        ...data,
+        votes: (rootComment.votes || 0) + 1,
+        userVoteValue: 1,
+      }));
+      await invokeGuestVote(supabase, rootComment.id, guestId, 1);
+    } else {
+      mutateRootComment((data: CommentType) => ({
+        ...data,
+        votes: (rootComment.votes || 0) - 1,
+        userVoteValue: 0,
+      }));
+      await invokeGuestVote(supabase, rootComment.id, guestId, 0);
+    }
+  }
+
+  async function invokeGuestVote(
+    supabase: any,
+    postId: number,
+    guestId: string,
+    value: number,
+  ): Promise<any> {
+    return supabase
+      .from("votes")
+      .upsert([{postId, guestid: guestId, isguest: true, value}])
+      .then(({data, error}) => {
+        if (error) {
+          console.error(error);
+          throw error;
+        }
+        return data;
+      });
+  }
+  
   return (
     <button
       className={`${styles.topButton} btn square`}
