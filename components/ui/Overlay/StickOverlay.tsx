9d49c2b62a6febb3d395866f80aba26643121de5
Sachin Kesiraju <kesirajusachin@gmail.com>
2023-12-20 12:56:05 -0800

    PW-117: Styled sticky overlay

--- components/ui/Overlay/StickOverlay.tsx
+++ components/ui/Overlay/StickOverlay.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { useState } from "react";
 import styles from "@/components/_styles/stickyOverlay.module.scss";
 
 interface StickyOverlayProps {
@@ -6,14 +7,29 @@ interface StickyOverlayProps {
 }
 
 const StickyOverlay: React.FC<StickyOverlayProps> = ({ count }) => {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <div className={styles.container}>
-      <div>
-        <p className={styles.remainCount}>{3 - count} Free Articles Left This Month</p>
-        <p className={styles.description}>Start your 14 day free trial for unilimited access</p>
-      </div>
-      <Link href="/subscribe">Subscribe</Link>
-    </div>
+    <>
+      {isOpen && (
+        <div className={styles.container}>
+          <button className={styles.closeButton} onClick={handleClose}>X</button>
+          <div className={styles.bannerContent}>
+            <div>
+              <p className={styles.remainCount}>{3 - count} Free Articles Left This Month</p>
+              <p className={styles.description}>Start your 14 day free trial for unlimited access</p>
+            </div>
+            <div className={styles.subscribeButton}>
+              <Link href="/subscribe">Subscribe</Link>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
